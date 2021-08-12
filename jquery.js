f37f7596f828dcf2a379d958d530aa399951880e
Prasad <panchal418@gmail.com>
2021-08-12 23:55:32 +0530

    user dropdown manu create

---
+++ jquery.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+$(".dropdown").hide();
+$(".inner").click(function(){
+  if($(this).parent().hasClass('active')){
+    $(this).parent().removeClass('active');
+    $('.dropdown').slideUp();
+  }else{
+    $(this).parent().addClass('active');
+    $('.inner').parent().removeClass('active').children('.dropdown').slideUp();
+    $(this).siblings().slideDown().parent().addClass('active');
+  }
+});
+$(".drop-click").click(function(){
+  $(".dropdown-items").toggleClass('show-dropdown')
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Document ready End
+});
